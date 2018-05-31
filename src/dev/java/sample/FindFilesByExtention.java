6d28ab1fbbc9befce91d254ec9a92666acc610a2
Devin <Devin@vWin10>
2018-05-31 06:54:38 -0700

    initialize

---
+++ src/dev/java/sample/FindFilesByExtention.java
@@ -0,0 +1,25 @@
+package dev.java.sample;
+import java.io.File;
+import java.io.FilenameFilter; 
+public class FindFilesByExtention {
+
+	
+    public static void main(String a[]){
+        File file = new File("C:\\Users\\Devin\\eclipse\\jee-oxygen\\eclipse\\readme");
+        String[] files = file.list(new FilenameFilter() {
+             
+            @Override
+            public boolean accept(File dir, String name) {
+               // if(name.toLowerCase().endsWith(".csv")){
+            	if(name.toLowerCase().endsWith(".html")){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        for(String f:files){
+            System.out.println(f);
+        }
+    }
+}
