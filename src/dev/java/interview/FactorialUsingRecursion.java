49ed5ee6558aa91e8f2460baaf499f7f00d705cd
Devin <devindbui@gmail.com>
2020-01-08 10:25:15 -0800

    commit test for atom

--- src/dev/java/interview/FactorialUsingRecursion.java
+++ src/dev/java/interview/FactorialUsingRecursion.java
@@ -1,15 +1,16 @@
 package dev.java.interview;
+//package may not work
 
 public class FactorialUsingRecursion {
-	 
+
     public static void main(String[] args) {
         System.out.println("5! = " + factorial(5));
     }
-    
+
     private static long factorial(int num){
         if(num==1)
             return 1;
-        else 
+        else
             return num*factorial(num-1);
     }
 }
